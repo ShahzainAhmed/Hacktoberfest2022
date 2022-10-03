04bb461b9608240ee85ff850e95d93f35ccecd1a
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-10-03 19:58:50 +0500

    replaced and added the comments for better understanding

--- scaling function/main.js
+++ scaling function/main.js
@@ -1,13 +1,7 @@
-
-
+// created a function to change the text 
 function changeThetext2()
 {
-//change the paragraph text
+
+// changing the text of paragraph
 document.getElementById('paragraph2').innerHTML='changing the text';
-
-
-
 }
-
-
-
